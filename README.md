1ac66d4059bec6b7e9a53613be5585d2bf2ca6c3
XabierArrieta <xabierarrietachango@gmail.com>
2021-02-08 17:33:25 +0100

    updates

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
 # geospatial-project
+
+![Spot](/Images/spot.png)
+
+Este es un proyecto que lo he realizado durante el Bootcamp de Data Anatlytics en [Ironhack](https://www.ironhack.com/es/data-analytics) Madrid.
+
+# Obejtivo
+
+El objetivo de este proyecto es determinar la ubicación perfecta de una compañía de videojuegos en base a unos criterios:
+- A los diseñadores les gusta ir a charlas de diseño y compartir conocimientos. Debe haber algunas empresas cercanas que también diseñen.
+- El 30% del personal de la empresa tiene al menos 1 hijo.
+- A los desarrolladores les gusta estar cerca de nuevas empresas tecnológicas exitosas que hayan recaudado al menos 1 millón de dólares.
+- A los ejecutivos les gusta mucho Starbucks. Asegúrese de que haya un Starbucks no muy lejos.
+- Los gerentes de cuentas necesitan viajar mucho.
+- Todos en la empresa tienen entre 25 y 40 años, dales un lugar para ir de fiesta.
+- El CEO es vegano.
+- Si quieres hacer feliz al encargado de mantenimiento, un estadio de baloncesto debe estar a unos 10 km.
+- El perro de la oficina, "Dobby", necesita un peluquero todos los meses. Asegúrese de que haya uno no demasiado lejos.
+
+
+# Proceso
+
+Para conseguir mi objtivo, comienzo con un dataset de [Crunchbase] (https://www.crunchbase.com/) que contiene información de mas de 18000 compañias. 
+Continuo limpiando el dataset y eniqueciendola añadiendo información con la API de Foursquare.
+Finalmente, filtro los datos según los criterios establecidos y selecciono la ubicación de la companía y así satisfacer a la mayoría de empleados, incluyendo un mapa interactivo usando Folium.
+
+# Referencias
+
+[Crunchbase](https://data.crunchbase.com/docs)
+[Fursquare API](https://developer.foursquare.com/)
---
+++ src/funcionesok.py
@@ -0,0 +1,73 @@
+import pandas as pd
+import geopandas as gpd
+import sys
+from dotenv import load_dotenv
+import os
+import requests
+import json
+from functools import reduce
+import operator
+from cartoframes.viz import Map, Layer, popup_element
+from haversine import haversine
+from pymongo import MongoClient,GEOSPHERE
+import shapely.geometry
+
+
+donde = input
+def geocode(address):
+    """
+    Metiendo un dirección se obtiene las coordenadas
+    """
+    data = requests.get(f"https://geocode.xyz/{address}?json=1").json()
+    try:
+        return {
+            "type":"Point",
+            "coordinates":[float(data["latt"]),float(data["longt"])]}
+    except:
+        return data                        
+                           
+                           
+def geo_api (empresa):
+    load_dotenv()
+    tok1 = os.getenv("tok1")
+    tok2 = os.getenv("tok2")
+    url_query = 'https://api.foursquare.com/v2/venues/explore' 
+          
+    criterio = input("¿Qué quieres buscar? \n")
+          
+    params = {"client_id" : tok1,
+              "client_secret" : tok2,
+              "v": "20180323",
+              "ll": f"60.17212, 24.94519",
+              "query":f"{criterio}",
+              "limit": 10
+            }
+    
+    resp = requests.get(url = url_query, params=params)
+    data = json.loads(resp.text)
+    decoding_data = data.get ("response")
+    decoded = decoding_data.get("groups")[0]
+    items = decoded.get("items")
+    
+    mapa_nombre = ["venue", "name"]
+    m_latitude = ["venue", "location", "lat"]
+    m_longitude = ["venue", "location", "lng"]
+    
+    def getFromDict(diccionario,mapa):
+        return reduce(operator.getitem,mapa,diccionario)
+    
+    list_items =[]
+    for dic in items:
+        items_dict = {}
+        items_dict["name"] = getFromDict(dic,mapa_nombre)
+        items_dict["latitud"] = getFromDict(dic,m_latitude)
+        items_dict["longitud"] = getFromDict(dic,m_longitude)
+        
+        list_items.append(items_dict)
+        
+        dataframefinal = pd.DataFrame(list_items)
+        gdf = gpd.GeoDataFrame(dataframefinal,geometry = gpd.points_from_xy(dataframefinal.longitud, dataframefinal.latitud))
+        Map(Layer(gdf, popup_hover = [popup_element(f"name",{criterio})]))
+    
+    return gdf
+    
